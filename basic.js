42117c92f9cfff7f4b99d86114a9947a43583fef
ShuninYu <92670725+ShuninYu@users.noreply.github.com>
2025-02-15 21:35:45 +0800

    通过js引入字体css

--- basic.js
+++ basic.js
@@ -1,2 +1,23 @@
 /** js document */
 
+// 动态加载CSS文件
+(function() {
+    //字体css
+    var cssUrl = 'https://chinese-fonts-cdn.deno.dev/packages/lxgwwenkai/dist/LXGWWenKai-Regular/result.css'; // 替换为你的CSS URL
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = cssUrl;
+    
+    // 兼容旧版浏览器获取<head>
+    var head = document.head || document.getElementsByTagName('head')[0];
+    
+    // 避免重复添加（可选）
+    var existingLinks = head.getElementsByTagName('link');
+    for (var i = 0; i < existingLinks.length; i++) {
+        if (existingLinks[i].href === cssUrl) {
+            return; // 如果已存在相同CSS链接，则不再添加
+        }
+    }
+    
+    head.appendChild(link);
+})();
